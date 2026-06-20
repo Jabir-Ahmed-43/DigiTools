1b68a906578e2deb4a0ebb8df684e1a5bd2ee14f
Jabir <jabir.ahmed484@gmail.com>
2026-06-20 15:34:39 +0600

    Trustbar issue solved

--- src/components/Trustbar/TrustBar.jsx
+++ src/components/Trustbar/TrustBar.jsx
@@ -1,19 +1,19 @@
 const TrustBar = () => {
   return (
-    <div className="flex justify-evenly items-center bg-gradient-to-r from-[#4F39F6] to-[#9514FA] py-15 px-50 text-white font-display">
+    <div className="flex justify-evenly items-center bg-gradient-to-r from-[#4F39F6] to-[#9514FA] py-15 px-50 text-white font-display ">
       <div className="">
         <h1 className="text-4xl font-bold">50K+</h1>
-        <p>Active Users</p>
+        <p className="mt-3">Active Users</p>
       </div>
-<div className="h-12 w-[1px] bg-white/50" />
+      <div className="h-12 w-[1px] bg-white/50" />
       <div>
         <h1 className="text-4xl font-bold">200+</h1>
-        <p>Premium Tools</p>
+        <p className="mt-3">Premium Tools</p>
       </div>
-<div className="h-12 w-[1px] bg-white/50" />
+      <div className="h-12 w-[1px] bg-white/50" />
       <div>
         <h1 className="text-4xl font-bold">4.9</h1>
-        <p>Rating</p>
+        <p className="mt-3">Rating</p>
       </div>
     </div>
   );
