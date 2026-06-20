f85343b2a0b97b8c87766992ecda4295a54f5afd
Jabir <jabir.ahmed484@gmail.com>
2026-06-20 16:45:48 +0600

    Description with button added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import "./App.css";
 import Hero from "./components/Hero/Hero.jsx";
 import Navbar from "./components/Navbar/Navbar.jsx";
+import Description from "./components/Product Grid/Description.jsx";
+import ProductGrid from "./components/Product Grid/ProductGrid.jsx";
 import TrustBar from "./components/Trustbar/TrustBar.jsx";
 
 function App() {
@@ -9,6 +11,8 @@ function App() {
       <Navbar></Navbar>
       <Hero></Hero>
       <TrustBar></TrustBar>
+      <Description></Description>
+      <ProductGrid></ProductGrid>
     </>
   );
 }
---
+++ src/components/Product Grid/Description.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { myBtnStyle } from "../Gradient-color/Gradient";
+
+const Description = () => {
+  const [productsBtn, setProductBtn] = useState(true);
+
+  function handleBtn(value) {
+    // console.log(value);
+    
+    setProductBtn(value);
+  }
+
+  return (
+    <div className="text-center font-display">
+      <h1 className="text-[48px] font-bold p-4">Premium Digital Tools</h1>
+      <p className="pb-4">
+        Choose from our curated collection of premium digital products designed
+        to boost your productivity, streamline your workflow, and supercharge
+        your creativity.
+      </p>
+      <div className="flex items-center justify-center gap-4 font-bold text-[16px] border-1 border-gray-300 display: inline-block p-1 rounded-4xl shadow-xl/30">
+        <button
+          className={`p-4 ${productsBtn && myBtnStyle + 'rounded-3xl text-white'}`}
+          onClick={() => handleBtn(true)}
+        >
+          Products
+        </button>
+        <button
+          className={`p-4 ${!productsBtn && myBtnStyle + 'rounded-3xl text-white'}`}
+          onClick={() => handleBtn(false)}
+        >
+          Cart
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Description;
